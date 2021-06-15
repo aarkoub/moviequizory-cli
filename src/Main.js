723c768a714572c09acc629ab356bc4a8aaf134a
amel <aarkoub@github.com>
2021-06-16 00:57:05 +0200

    Refactor main component #SB5

--- src/Main.js
+++ src/Main.js
@@ -1,32 +1,118 @@
 import React, { Component } from "react";
+import { Route, HashRouter } from "react-router-dom";
 import { Flex, Box, Link, Button } from 'rebass';
-import Welcome from "./Welcome";
 
 class Main extends Component {
+
+  constructor(props) {
+    super(props);
+    this.api_host = process.env.REACT_APP_API_HOST_DEV;
+    this.state = {
+      error: undefined,
+      user: undefined
+    }
+  }
+
+  async componentDidMount() {
+    const response = await fetch(this.api_host + "users/create", { credentials: 'include' });
+    this.setState({ isLoaded: true })
+    if (response.error) {
+      this.setState({ error: response.error });
+    }
+    else {
+      this.setState({ user: response.json() })
+    }
+  }
+
   render() {
     return (
-      <Box>
-        <Flex
-          px={2}
-          color='white'
-          bg='Pink'
-          alignItems='center'>
-          <Link sx={{
-            display: 'inline-block',
-            fontWeight: 'bold',
-            px: 2,
-            py: 1,
-            color: 'inherit',
-          }} variant='nav' href='/'>
-            MovieQuizory
+      <HashRouter>
+        <Box>
+          <Flex
+            px={2}
+            color='white'
+            bg='Pink'
+            alignItems='center'>
+            <Link sx={{
+              display: 'inline-block',
+              fontWeight: 'bold',
+              px: 2,
+              py: 1,
+              color: 'inherit',
+            }} variant='nav' href='/'>
+              MovieQuizory
           </Link>
-        </Flex>
-        <Box id="content">
-          <Welcome/>
+          </Flex>
+          <Box id="content">
+            <Route exact path="/" component={Welcome} />
+            <Route path="/play" component={Quiz} />
+            <Route path="/gameover" component={Result}/>
+          </Box>
         </Box>
-      </Box>
+      </HashRouter>
     );
   }
 }
+
+class Quiz extends Component{
+  
+  constructor(props) {
+    super(props);
+    this.api_host = process.env.REACT_APP_API_HOST_DEV;
+    this.state = {
+        error: undefined,
+        quiz: undefined
+    }
+}
+
+  async componentDidMount() {
+    const response = await fetch(this.api_host + "quizzes/generate", { credentials: 'include' });
+      this.setState({ isLoaded: true })
+      if (response.error) {
+          this.setState({ error: response.error });
+      }
+      else {
+          this.setState({ quiz: response.json() })
+      }
+  }
+
+  render(){
+    return <div>Display quiz</div>
+  }
+}
+
+class Result extends Component{
+  render(){
+    return <div>Display results</div>
+  }
+
+}
+
+class Welcome extends Component {
+
+  constructor(props) {
+      super(props);
+      this.startGame = this.startGame.bind(this);
+  }
+
+  startGame(){
+      this.props.history.push("/play");
+  }
+
+
+  render() {
+      return (
+          <Box id='welcome'>
+              <Box id='rules'>
+                  Welcome to the quizz ! You'll be asked a series of "Yes or No" questions. Answer as many as you can in the allowed time ! Good luck !
+           </Box>
+              <Button color='pink' bg='black' onClick={this.startGame}>Start</Button>
+          </Box>
+
+      );
+  }
+
+}
+
 
 export default Main;
