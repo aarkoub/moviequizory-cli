a68785226187385d0d4a9d686586968e7bd2b26b
amel <aarkoub@github.com>
2021-06-16 13:04:46 +0200

    Add quiz content #SB2 #SB3

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Route, HashRouter } from "react-router-dom";
-import { Flex, Box, Link, Button } from 'rebass';
+import { Flex, Box, Link, Button, Card, Image } from 'rebass';
 
 class Main extends Component {
 
@@ -46,7 +46,7 @@ class Main extends Component {
           <Box id="content">
             <Route exact path="/" component={Welcome} />
             <Route path="/play" component={Quiz} />
-            <Route path="/gameover" component={Result}/>
+            <Route path="/gameover" component={Result} />
           </Box>
         </Box>
       </HashRouter>
@@ -54,35 +54,78 @@ class Main extends Component {
   }
 }
 
-class Quiz extends Component{
-  
+class Quiz extends Component {
+
   constructor(props) {
     super(props);
     this.api_host = process.env.REACT_APP_API_HOST_DEV;
+    this.checkAnswer = this.checkAnswer.bind(this);
     this.state = {
-        error: undefined,
-        quiz: undefined
+      error: undefined,
+      quiz: {
+        questions: [{
+          content: "",
+          actor: { pictureURL: "" },
+          movie: { pictureURL: "" }
+        }]
+      },
+      current_question_ind: 0,
+      curr_score: 0
     }
-}
+  }
 
   async componentDidMount() {
     const response = await fetch(this.api_host + "quizzes/generate", { credentials: 'include' });
-      this.setState({ isLoaded: true })
-      if (response.error) {
-          this.setState({ error: response.error });
-      }
-      else {
-          this.setState({ quiz: response.json() })
-      }
+    this.setState({ isLoaded: true })
+    if (response.error) {
+      this.setState({ error: response.error });
+    }
+    else {
+      const quiz = await response.json();
+      this.setState({ quiz: quiz })
+    }
   }
 
-  render(){
-    return <div>Display quiz</div>
+  checkAnswer(response) {
+    if (response === this.state.quiz.questions[this.state.current_question_ind].true) {
+      this.setState({ curr_score: this.state.curr_score + 1 })
+    }
+    if (this.state.current_question_ind === this.state.current_question_ind + 1 == this.state.quiz.questions.length) {
+      this.props.history.push("/gameover");
+    }
+    else {
+      this.setState({ current_question_ind: this.state.current_question_ind + 1 })
+    }
+  }
+
+  render() {
+    return <Box>
+      <Box>{this.state.quiz.questions[this.state.current_question_ind].content}</Box>
+      <Flex>
+        <Box>
+          <Card>
+            <Image
+              sx={{ width: ['100%', '50%'], borderRadius: 8 }}
+              src={this.state.quiz.questions[this.state.current_question_ind].actor.pictureURL} />
+          </Card>
+          <Button color='pink' bg='black' onClick={()=> this.checkAnswer(true)}>Yes</Button>
+        </Box>
+        <Box>
+          <Card>
+            <Image
+              sx={{ width: ['100%', '50%'], borderRadius: 8 }}
+              src={this.state.quiz.questions[this.state.current_question_ind].movie.pictureURL} />
+          </Card>
+          <Button color='pink' bg='black' onClick={()=> this.checkAnswer(false)}>No</Button>
+        </Box>
+      </Flex>
+      <Box>Score: {this.state.curr_score}</Box>
+    </Box>
   }
 }
 
-class Result extends Component{
-  render(){
+class Result extends Component {
+  render() {
     return <div>Display results</div>
   }
 
@@ -91,25 +134,25 @@ class Result extends Component{
 class Welcome extends Component {
 
   constructor(props) {
-      super(props);
-      this.startGame = this.startGame.bind(this);
+    super(props);
+    this.startGame = this.startGame.bind(this);
   }
 
-  startGame(){
-      this.props.history.push("/play");
+  startGame() {
+    this.props.history.push("/play");
   }
 
 
   render() {
-      return (
-          <Box id='welcome'>
-              <Box id='rules'>
-                  Welcome to the quizz ! You'll be asked a series of "Yes or No" questions. Answer as many as you can in the allowed time ! Good luck !
+    return (
+      <Box id='welcome'>
+        <Box id='rules'>
+          Welcome to the quizz ! You'll be asked a series of "Yes or No" questions. Answer as many as you can in the allowed time ! Good luck !
            </Box>
-              <Button color='pink' bg='black' onClick={this.startGame}>Start</Button>
-          </Box>
+        <Button color='pink' bg='black' onClick={this.startGame}>Start</Button>
+      </Box>
 
-      );
+    );
   }
 
 }
